c4f89d5d41c39bf3bbf883a853838aec8140af6d
Matija Radomirovic <98714697+rad22669@users.noreply.github.com>
2022-03-02 21:56:40 +0100

    Update syncGit.cmd

--- syncGit.cmd
+++ syncGit.cmd
@@ -1,6 +1,10 @@
 REM https://stackoverflow.com/questions/203090/how-do-i-get-current-datetime-on-the-windows-command-line-in-a-suitable-format
-For /f "tokens=1-3 delims=. " %%a in ('date /t') do (set mydate=%%c%%b%%a)
+FOR /F "Tokens=1-3 Delims=/ " %%A In ('RoboCopy/NJH /L "\|" Null'
+) Do Set "mydate=%%A_%%B_%%C" & GoTo :Break
+:Break
+Set "mydate=%mydate:~2%"
 git add -A
 git commit -a -m "Commit %mydate%"
 git pull
 git push
+Timeout -1
